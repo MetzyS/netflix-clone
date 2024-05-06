9d2f4d9cd0988c6ca8fab6372e581ff2a1abaf78
metzys <contact@metzys.net>
2024-05-06 21:11:10 +0200

    style

--- src/components/ui/DefaultLink.tsx
+++ src/components/ui/DefaultLink.tsx
@@ -2,7 +2,10 @@ import { Link } from "react-router-dom";
 
 const DefaultLink = (props: { text: string; className?: string }) => {
   return (
-    <Link to="login" className={`btn-default ring-default ${props.className}`}>
+    <Link
+      to="login"
+      className={`btn-default btn-primary ring-default ${props.className}`}
+    >
       {props.text}
     </Link>
   );
