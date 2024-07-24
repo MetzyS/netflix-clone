14d4b8b75053e3f86f667938c7709f5ff87b69f5
metzys <contact@metzys.net>
2024-07-24 07:27:57 +0200

    ajout skeleton durant chargement image + fix style

--- src/components/ShowMovies/CustomSection/EpisodeDetail.tsx
+++ src/components/ShowMovies/CustomSection/EpisodeDetail.tsx
@@ -1,6 +1,17 @@
+import { useEffect, useState } from "react";
 import { Episode } from "../../../types/data";
 
 const EpisodeDetail = (props: { episode: Episode; minutes: string }) => {
+  const [imageIsLoaded, setImageIsLoaded] = useState(false);
+  const image = new Image();
+  useEffect(() => {
+    image.src = `https://image.tmdb.org/t/p/w154/${props.episode.still_path}.jpg`;
+    image.onload = () => {
+      setImageIsLoaded(true);
+      console.log("trued");
+    };
+  }, [props.episode.still_path]);
+
   return (
     <div className="flex flex-col items-center justify-center md:justify-normal md:items-start md:flex-row md:py-6 md:px-8 gap-4 md:rounded-md border border-transparent border-b-neutral-700">
       <h4 className="md:hidden line-clamp-1 text-center mt-2 md:mt-0">
@@ -10,17 +21,21 @@ const EpisodeDetail = (props: { episode: Episode; minutes: string }) => {
         <span className="text-xl self-center p-2">
           {props.episode.episode_number}
         </span>
-        <div
-          className="block w-[154px] aspect-video m-auto md:m-0 bg-neutral-700 animate-pulse"
-          style={{
-            background: `url(https://image.tmdb.org/t/p/w154/${props.episode.still_path}.jpg)`,
-            backgroundPosition: "center",
-            backgroundSize: "154px 100px",
-            backgroundRepeat: "no-repeat",
-          }}
-        ></div>
+        {imageIsLoaded ? (
+          <div
+            className="block w-[154px] aspect-video m-auto md:m-0"
+            style={{
+              backgroundImage: `url(https://image.tmdb.org/t/p/w154/${props.episode.still_path}.jpg)`,
+              backgroundPosition: "center",
+              backgroundSize: "154px 100px",
+              backgroundRepeat: "no-repeat",
+            }}
+          ></div>
+        ) : (
+          <div className="block w-[154px] h-full aspect-video m-auto md:m-0 bg-neutral-700 animate-pulse rounded-sm"></div>
+        )}
       </div>
-      <div className="pl-2 grid">
+      <div className="pl-2 grid w-full">
         <div className="md:flex md:justify-between md:mb-2 hidden">
           <h4 className="max-w-[85%] line-clamp-1">{props.episode.name}</h4>
           <span>{`${props.episode.runtime} ${props.minutes}`}</span>
