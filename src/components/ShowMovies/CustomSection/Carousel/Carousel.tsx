f37e3a8dd6f7a46c4f256863011ecbe65339cf41
metzys <contact@metzys.net>
2024-08-17 15:48:26 +0200

    tests

--- src/components/ShowMovies/CustomSection/Carousel/Carousel.tsx
+++ src/components/ShowMovies/CustomSection/Carousel/Carousel.tsx
@@ -9,25 +9,16 @@ const Carousel = (props: {
   screenSize: number;
 }) => {
 
-  // CAROUSEL REF & READY STATE
-  const [carouselIsReady, setCarouselIsReady] = useState<boolean>(false);
-  const carouselRef = useRef<HTMLUListElement>(null)
-  useEffect(() => {
-    if (carouselRef.current) {
-      setCarouselIsReady(true);
-    }
-
-  }, [carouselRef])
-
-  const [carouselIsActivated, setCarouselIsActivated] = useState<boolean>(false);
-  const [carouselInfo, setCarouselInfo] = useState<{
+  type CarouselInfoType = {
     posX: number,
     screenSize: number,
     thumbnailSize: number,
     baseOffset: number,
     thumbnailCount: number,
     maxSize: number
-  }>({
+  }
+
+  const [carouselInfo, setCarouselInfo] = useState<CarouselInfoType>({
     posX: 0,
     screenSize: props.screenSize,
     thumbnailSize: 0,
@@ -35,10 +26,98 @@ const Carousel = (props: {
     thumbnailCount: props.data.length,
     maxSize: 0,
   })
+  // CAROUSEL REF & READY STATE
+  const [carouselIsReady, setCarouselIsReady] = useState<boolean>(false);
+  const carouselRef = useRef<HTMLUListElement>(null)
 
-  const handleCarouselActivation = () => {
-    if (carouselIsReady && !carouselIsActivated) {
+
+  useEffect(() => {
+    if (carouselRef.current) {
+      setCarouselIsReady(true);
+      const thumbnailWidth = carouselRef.current.children[0].clientWidth;
+      console.log(`thumbnailWidth = ${thumbnailWidth}`)
+      console.log(`offset: `)
+      setCarouselInfo((prevState) =>
+        // let newState = { ...carouselInfo, thumbnailSize: thumbnailWidth })
+        setCarouselInfo({ ...carouselInfo, maxSize: -Math.abs(thumbnailWidth * carouselInfo.thumbnailCount) })
+    }
+
+  }, [carouselRef])
+
+  const [carouselIsActivated, setCarouselIsActivated] = useState<boolean>(false);
+
+
+  // useEffect(() => {
+  //   if (carouselIsReady && carouselRef.current) {
+  //     // ThumbnailSize (thumbnail width)
+  //     const thumbnailWidth = carouselRef.current.children[0].clientWidth
+  //     setCarouselInfo({ ...carouselInfo, thumbnailSize: thumbnailWidth })
+
+  //     // MaxSize (thumbnail count * thumbnail width)
+  //     setCarouselInfo({ ...carouselInfo, maxSize: -Math.abs(thumbnailWidth * carouselInfo.thumbnailCount) })
+  //     console.log(thumbnailWidth)
+  //     // console.log(carouselInfo);
+
+  //   }
+  // }, [props.screenSize, props.data])
+
+
+  const carouselCalculations = (carouselInfo: CarouselInfoType) => {
+    const displayableWidth = carouselInfo.screenSize - carouselInfo.baseOffset;
+    console.log(`displayableWidth = ${displayableWidth} , thumbnailSize: ${carouselInfo.thumbnailSize}`)
+    const visibleThumbnails = Math.floor(displayableWidth / carouselInfo.thumbnailSize);
+    const offset = visibleThumbnails * (carouselInfo.screenSize * 0.01);
+    const displayableWidthWithoutGaps = displayableWidth - offset;
+    const displayableThumbnails = Math.floor(
+      (displayableWidthWithoutGaps / carouselInfo.thumbnailSize))
+    const visibleThumbnailsRaw = displayableWidthWithoutGaps / carouselInfo.thumbnailSize;
+    const maxOffset = (carouselInfo.thumbnailCount - 1) * 10;
+    const maxScrollX = -Math.abs(carouselInfo.thumbnailSize * carouselInfo.thumbnailCount + maxOffset);
+
+    console.log({
+      displayableWidth, visibleThumbnails, offset, displayableWidthWithoutGaps, displayableThumbnails, visibleThumbnailsRaw, maxOffset, maxScrollX
+    })
+
+    return {
+      displayableWidth, visibleThumbnails, offset, displayableWidthWithoutGaps, displayableThumbnails, visibleThumbnailsRaw, maxOffset, maxScrollX
+    }
+  }
+
+  const handleCarousel = (signal: string) => {
+    if (!carouselIsActivated && carouselIsReady) {
       setCarouselIsActivated(true);
+    }
+    let carouselCalc = carouselCalculations(carouselInfo);
+    // console.log(carouselInfo);
+
+    switch (signal) {
+
+      case "NEXT": {
+
+        const newPos = carouselInfo.posX - (carouselInfo.thumbnailSize * carouselCalc.displayableThumbnails + carouselCalc.offset)
+
+        if ((newPos - carouselInfo.thumbnailSize * carouselCalc.displayableThumbnails) < carouselInfo.maxSize) {
+          setCarouselInfo({ ...carouselInfo, posX: carouselCalc.maxScrollX + carouselCalc.visibleThumbnailsRaw * carouselInfo.thumbnailSize + carouselCalc.offset })
+          break;
+        }
+        if ((newPos == carouselCalc.maxScrollX)) {
+          setCarouselInfo({ ...carouselInfo, posX: 0 });
+          break;
+        }
+
+        // console.log(`newPos = ${carouselInfo.posX}`);
+        setCarouselInfo({ ...carouselInfo, posX: newPos });
+        break;
+      }
+
+      case "PREV": {
+        console.log("TEST")
+        break;
+      }
+      default: {
+        console.log("default");
+        break;
+      }
     }
   }
 
@@ -46,14 +125,15 @@ const Carousel = (props: {
   return (
     <div className="ml-4 lg:ml-12">
       <button
-        className={`carousel-btn rounded-tl-md rounded-bl-md left-0 group ${carouselIsActivated ? "block bg-black/50 hover:bg-black/90" : ""
+        className={`carousel-btn rounded-tl-md rounded-bl-md left-4 group ${carouselIsActivated ? "block bg-black/50 hover:bg-black/90" : ""
           }`}
+        onClick={() => handleCarousel("PREV")}
       >
         <MdArrowBackIosNew className="text-white size-8 transition-all p-1 group-hover:p-0" />
       </button>
       <button
         className="carousel-btn rounded-tr-md rounded-br-md block right-0 text-transparent bg-black/50 hover:bg-black/90 group"
-        onClick={handleCarouselActivation}
+        onClick={() => handleCarousel("NEXT")}
       >
         <MdArrowBackIosNew className="rotate-180 text-white size-8 transition-all p-1 group-hover:p-0" />
       </button>
